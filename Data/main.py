3c9e2e9fcae7512c95feab4a372bae0041de77dc
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2023-04-30 22:43:09 -0700

    Update main.py

--- Data/main.py
+++ Data/main.py
@@ -2,22 +2,34 @@
 import matplotlib
 import csv
 from matplotlib import pyplot as plt
+import numpy as np
 
 #interpreter
 f=open('data.txt',"r")
 lines=f.readlines()
 crimes=[]
 years=[]
-for y in lines:
-    crimes.append(y.split(' ')[1])
-for x in lines:
-    years.append(x.split(' ')[0])
-x=years
-default_x_ticks = range(len(x))
-plt.xticks(default_x_ticks, x)
-    
 
-plt.plot(crimes)
+#reading.moment
+for h in lines:
+    crimes.append(h.split(' ')[3])
+for k in lines:
+    years.append(k.split(' ')[0])
+
+#axis setup
+k=years
+default_k_ticks = range(len(k))
+plt.xticks(default_k_ticks, k)
+
+h=crimes
+default_h_ticks=range(len(k))
+plt.yticks(default_h_ticks,h)
+
+#labeling.exe
+'''plt.xlabel("years")
+plt.ylabel("number of crimes, in millions, in the USA")'''
+
+#moment of coolness
 plt.plot(crimes)
 plt.show()
 f.close()
